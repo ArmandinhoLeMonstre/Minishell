b9ad3255d13308d3f45d04ee68968dcaeddc5ddc
Armand Mititelu <armitite@shi-r7-p1.s19.be>
2024-11-23 18:23:04 +0100

    corr exit avec arg non numerique

--- src/exit.c
+++ src/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
+/*   By: armitite <armitite@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 12:51:17 by mucabrin          #+#    #+#             */
-/*   Updated: 2024/11/22 19:51:16 by armitite         ###   ########.fr       */
+/*   Updated: 2024/11/23 18:22:10 by armitite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,38 @@ static void	ft_exit(char *arg)
 	}
 }
 
-// static long	ft_atol(const char *str, int *overflow)
-// {
-// 	int		i;
-// 	int		sign;
-// 	long	num;
-// 	long	buf;
+static long	ft_atol(const char *str, int *overflow)
+{
+	int		i;
+	int		sign;
+	long	num;
+	long	buf;
 
-// 	i = 0;
-// 	sign = 1;
-// 	num = 0;
-// 	buf = 0;
-// 	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
-// 		i++;
-// 	if (str[i] == '+' || str[i] == '-')
-// 		if (str[i++] == '-')
-// 			sign *= -1;
-// 	while (str[i] >= '0' && str[i] <= '9' && !*overflow)
-// 	{
-// 		num = (num * 10) + (str[i++] - 48) * sign;
-// 		if ((buf > num && sign > 0) || (buf < num && sign < 0))
-// 			*overflow = 1;
-// 		buf = num;
-// 	}
-// 	if ((str[i] && (str[i] < 9 || str[i] > 13 || (str[i] != 32))))
-// 		*overflow = 1;
-// 	return (num);
-// }
+	i = 0;
+	sign = 1;
+	num = 0;
+	buf = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign *= -1;
+	while (str[i] >= '0' && str[i] <= '9' && !*overflow)
+	{
+		num = (num * 10) + (str[i++] - 48) * sign;
+		if ((buf > num && sign > 0) || (buf < num && sign < 0))
+			*overflow = 1;
+		buf = num;
+	}
+	if ((str[i] && (str[i] < 9 || str[i] > 13 || (str[i] != 32))))
+		*overflow = 1;
+	return (num);
+}
 
 void	exit_built(char **token)
 {
 	char	*arg;
-	//long	n;
+	long	n;
 	int		overflow;
 
 	if (!token[1])
@@ -73,7 +73,7 @@ void	exit_built(char **token)
 	}
 	arg = ft_strdup(token[1]);
 	overflow = 0;
-	//n = ft_atol(arg, &overflow);
+	n = ft_atol(arg, &overflow);
 	if (overflow)
 		ft_exit(arg);
 	if (token[2])
